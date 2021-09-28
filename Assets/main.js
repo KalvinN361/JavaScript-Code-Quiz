cb582182f319b6e80982efac37d26120509397be
Kalvin Nguyen <kalvin.win59@gmail.com>
2021-09-28 14:45:25 -0500

    2

---
+++ Assets/main.js
@@ -0,0 +1,13 @@
+// time variables
+var currentQuestionIndex = 0;
+var time = 60;
+var timerRight;
+
+// main variables
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
