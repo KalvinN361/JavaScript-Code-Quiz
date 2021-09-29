c02a5552d0dc9eae8d4e777d59e60c9d0a4b2c10
Kalvin Nguyen <kalvin.win59@gmail.com>
2021-09-28 19:38:12 -0500

    css error fix maybe

--- Assets/scores.js
+++ Assets/scores.js
@@ -1,24 +1,19 @@
 function printHighscores() {
-    // pulls highscroes from local storage which we imprinted in the main js. 
+   
     var highScores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-  
-    // This will have the scores go from highest to lowest
     highScores.sort(function(a, b) {
       return b.score - a.score;
     });
     
 
-    // This function will create a list for each of the high scores which will be ordered above
     highScores.forEach(function(score) {
       var liTag = document.createElement("li");
-      liTag.textContent = score.initials + " - " + score.score;
-  
-      // this sends the li tags to be appended on page. 
+      liTag.textContent = score.initials + " - " + score.score; 
       var olEl = document.getElementById("highscores");
       olEl.appendChild(liTag);
     });
   }
-//   this function will clear the scores fromt he highscores list
+
   function clearHighscores() {
     window.localStorage.removeItem("highscores");
     window.location.reload();
@@ -26,5 +21,5 @@ function printHighscores() {
   
   document.getElementById("clear").onclick = clearHighscores;
   
-  // this function will runt he first function
+  
   printHighscores();
